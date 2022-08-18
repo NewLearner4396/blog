6992ccb9a90b2397c507f394194aa637ca49edfb
NewLearner4396 <NewLearner4396@users.noreply.github.com>
2022-08-18 20:46:47 +0800

    test_git

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "Land of Fragment",
-  description: 'Welcome to my little world .',
+  description: 'Welcome to my little world.',
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
